a635a94e2342adb083b3326d943e290b6b230484
Cédryc Ruth <ceruth@epfc.eu>
2025-10-21 10:03:33 +0200

    Profile update

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,26 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .forms import UserSignUpForm
+from .forms import UserUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+class UserUpdateView(UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("accounts:user-profile")
+    template_name = "user/update.html"
+
+    def test_func(self):
+        pkInURL = self.kwargs['pk']
+        return self.request.user.is_authenticated and self.request.user.id==pkInURL or self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Vous n'avez pas l'autorisation d'accéder à cette page!")
+        return redirect('accounts:user-profile')
 
 class UserSignUpView(UserPassesTestMixin, CreateView):
     form_class = UserSignUpForm
@@ -27,6 +42,6 @@ def profile(request):
         "nl": "Nederlands",
     }
 
-    return render(request, 'profile.html', {
+    return render(request, 'user/profile.html', {
         "user_language" : languages[request.user.usermeta.langue],
     })
